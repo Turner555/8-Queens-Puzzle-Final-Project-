fa9ad1a87aef75dc14ec01c572037296c717c355
Turner Alford <tjalford@seu.edu>
2026-04-09 15:13:14 -0400

    Realized I should not put any recursion inside this check function and moved the input of the piece outside the function. Now the function simply returns True or False.

--- check.py
+++ check.py
@@ -1,7 +1,4 @@
-def check(placed, current_piece):
-    current_piece = input("What colum and row have you placed the queen on (e.g. 1 3): ")
-    column = (int(current_piece.split()[0]) - 1)
-    row = int(current_piece.split()[1])
+def check(placed, column, row):
     for i in range(len(placed)):
         if placed[i] > 0:
             if placed[i] == row:
@@ -10,7 +7,12 @@ def check(placed, current_piece):
                 return False
             elif abs(i - column) == abs(placed[i] - row):
                 return False
-    placed[column] = row
-    check(placed, current_piece)
+    return True
+    #placed[column] = row
 
-print(check([0, 0, 0, 0, 0, 0, 0, 0], 0))
+    #check(placed, current_piece)
+
+current_piece = input("What colum and row have you placed the queen on (e.g. 1 3): ")
+column = (int(current_piece.split()[0]) - 1)
+row = int(current_piece.split()[1])
+print(check([0, 0, 0, 0, 0, 0, 0, 0], column, row))
