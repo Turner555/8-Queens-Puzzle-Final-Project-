aab75a6feb82810f0cbc7532ddd317e59eead4fc
Turner Alford <tjalford@seu.edu>
2026-04-09 15:18:19 -0400

    Starting to think about how to do the recursion separate from my check function. Sorry that these first three commits have a lot of changes, it took me a minute to figure out how to set this up but I also started my code on github while I waited. These are the first few steps I took there but will start to move everything over to here.

--- check.py
+++ check.py
@@ -1,18 +1,26 @@
 def check(placed, column, row):
-    for i in range(len(placed)):
-        if placed[i] > 0:
-            if placed[i] == row:
-                return False
-            elif i == column:
-                return False
-            elif abs(i - column) == abs(placed[i] - row):
-                return False
-    return True
-    #placed[column] = row
+        for i in range(len(placed)):
+            if placed[i] > 0:
+                if placed[i] == row:
+                    return False
+                elif i == column:
+                    return False
+                elif abs(i - column) == abs(placed[i] - row):
+                    return False
+        return True
 
-    #check(placed, current_piece)
+def place_queens(placed, current_piece, future_queens):
+    current_piece = input("What colum and row have you placed the queen on (e.g. 1 3): ")
+    column = (int(current_piece.split()[0]) - 1)
+    row = int(current_piece.split()[1])
+    validity = check(placed, column, row)
+    if validity is True:
+         placed[column] = row
+    else:
+         print("This placement is not valid")
 
-current_piece = input("What colum and row have you placed the queen on (e.g. 1 3): ")
-column = (int(current_piece.split()[0]) - 1)
-row = int(current_piece.split()[1])
-print(check([0, 0, 0, 0, 0, 0, 0, 0], column, row))
+
+    return placed
+
+
+print(place_queens([0, 0, 0, 0, 0, 0, 0, 0], 0, 0))
