0bd1aa120a30b852ee78d85905493c6bc48f6d62
Turner Alford <tjalford@seu.edu>
2026-04-20 15:59:37 -0400

    Updated board.py to correctly print a visualization of the board. I plan to implement this into main.py.

--- board.py
+++ board.py
@@ -1,15 +1,19 @@
 _board = ["[]"] * 8
 
-def board(placed, row):
-    if row == 8:
+def board(placed, row, column):
+    if row == 9:
         return 1
     
-    for column in range(len(placed)):
-        if placed[column] == row:
-            _board[column] = "Q"
-            print(f"{" ".join(_board)}\n")
-            _board[column] = "[]"
-        
-    board(placed, row + 1)
+    if row in placed:
+        for column in range(len(placed)):
+            if placed[column] == row:
+                _board[column] = " Q"
+                print(f"{" ".join(_board)} ")
+                _board[column] = "[]"
+                break
+    else:
+        print(" ".join(_board))
+    
+    board(placed, row + 1, column)
 
-board([1, 0, 3, 0, 5, 0, 7, 0], 1)
+board([1, 0, 3, 0, 5, 0, 7, 0], 1, 0)
