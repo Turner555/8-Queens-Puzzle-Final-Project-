923a92fc7b870c64805b0c62c021696a097b1c7c
Turner Alford <tjalford@seu.edu>
2026-04-20 21:00:12 -0400

    Finished designingthe board and added it to main.py for a more interactive experience.

--- board.py
+++ board.py
@@ -1,19 +1,20 @@
-_board = ["[]"] * 8
+_board = ["[ ]"] * 8
 
-def board(placed, row, column):
+def make_board(placed, row, column):
     if row == 9:
-        return 1
+        return placed
     
     if row in placed:
         for column in range(len(placed)):
             if placed[column] == row:
-                _board[column] = " Q"
+                _board[column] = "[Q]"
                 print(f"{" ".join(_board)} ")
-                _board[column] = "[]"
+                _board[column] = "[ ]"
                 break
     else:
         print(" ".join(_board))
     
-    board(placed, row + 1, column)
+    make_board(placed, row + 1, column)
 
-board([1, 0, 3, 0, 5, 0, 7, 0], 1, 0)
+if __name__ == "__main__":
+    make_board([1, 0, 3, 0, 5, 0, 7, 0], 1, 0)
--- main.py
+++ main.py
@@ -1,55 +1,66 @@
 from _backtrack import place_queens
+from board import make_board
 
 def main(placed):
     #Get placement of queen from user
-    current_piece = input("What colum and row have you placed the queen on (e.g. 1 3): ")
+    current_piece = input("What colum and row do you want to place the queen on (ex. 1 3): ")
     column = (int(current_piece.split()[0]) - 1)
     #Guard against invalid column.
     if column < 0 or column > 7:
         print("Oops, it looks like this is not a valid column. Please re-enter a column and row between 1 and 8.")
         print(placed)
-        main(placed)
+        return main(placed)
     row = int(current_piece.split()[1])
     #Guard against invalid row.
     if row < 1 or row > 8:
         print("Oops, it looks like this is not a valid row. Please re-enter a column and row between 1 and 8.")
         print(placed)
-        main(placed)
+        return main(placed)
 
     #Check validity of placement
     result = place_queens(placed, column, row)
-    print(result)
+    #print(result)
 
     #Keep going if placement is valid and there are possible outcomes.
     if result[1] > 0 and result[1] != 100:
-        print(f"You are on the right track. There are {result[1]} possible solutions left.")
+        print(f"\n\nYou are on the right track! There are {result[1]} possible solutions left.")
         print("Please place your next queen.")
-        main(placed)
+        #Make a visual board in GUI
+        make_board(placed, 1, 0)
+        return main(placed)
 
     #Re-do last placement if there are no more valid solutions.
     elif result[1] == 0:
-        print("Oh no! This leads to a dead end.")
+        print("\n\nOh no! This leads to a dead end.")
         print("Please re-place your last queen.")
-        main(placed)
+        #Make a visual board in GUI
+        make_board(placed, 1, 0)
+        return main(placed)
 
     #Re-do last placement if it conflicts with the queens already on the board.
     elif result[1] == -1:
         print("Yikes! THis placement conflicts with queens already on the board")
         print("Please re-place your last queen.")
-        main(placed)
+        #Make a visual board in GUI
+        make_board(placed, 1, 0)
+        return main(placed)
 
     #Let user know they completed the puzzle and ask if they want to go again.
     elif result[1] == 100:
         print("Congrats on finding a way to complete the standoff!")
-        print("Would you like to try another route.")
+        #Make a visual board in GUI
+        make_board(placed, 1, 0)
+        print("\nWould you like to try another route?")
         again = input("y/n: ")
         if again == 'y':
-            main([0, 0, 0, 0, 0, 0, 0, 0])
+           return main([0, 0, 0, 0, 0, 0, 0, 0])
         elif again == 'n':
             return -1
 
 #Intro and initial call into the program
-print("Hello, and welcome to The Royal Standoff!")
-print("To begin, please place your first queen.")
+print("Hello, and welcome to The Royal Standoff!\n")
+print("To begin, please place your first queen.\n")
 board = [0, 0, 0, 0, 0, 0, 0, 0]
+#Make a visual board in GUI
+make_board(board, 1, 0)
 main(board)
